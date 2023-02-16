8dc581bef8acc0bb082e5855b3c26b9c97703d9d
myungsun <audtjs9987@naver.com>
2023-02-16 23:03:12 +0900

    feat: compose RecommendationCell

--- README/Cells/RecommendationCell.swift
+++ README/Cells/RecommendationCell.swift
@@ -8,10 +8,22 @@
 import UIKit
 
 class RecommendationCell: UICollectionViewCell {
-
+    // UILabel
+    @IBOutlet weak var titleLabel: UILabel!
+    
+    // UIView
+    @IBOutlet weak var containerView: UIView!
+    
+    // Constants
+    let VIEW_BORDER_WIDTH: CGFloat = 1
     override func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
+        
+        containerView.layer.borderWidth = VIEW_BORDER_WIDTH
+        containerView.layer.borderColor = ColorManager.shared.getChineseSilver().cgColor
     }
-
+    
+    func setData(title: String) {
+        titleLabel.text = title
+    }
 }
