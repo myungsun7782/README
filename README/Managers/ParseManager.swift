97a17cd395da7ac864e60673272afe5a1b0985bb
myungsun <audtjs9987@naver.com>
2023-02-16 23:01:38 +0900

    feat: develop ParseManager class

--- README/Managers/ParseManager.swift
+++ README/Managers/ParseManager.swift
@@ -6,3 +6,26 @@
 //
 
 import Foundation
+import SwiftyJSON
+
+enum SearchKey: String {
+    case RESULT = "result"
+}
+
+class ParseManager {
+    static let shared = ParseManager()
+    
+    private init() {}
+    
+    func parseRelationResultJSON(_ resultData: Data) -> [String]? {
+        let json = JSON(resultData)
+        let relationResult = json[SearchKey.RESULT.rawValue].arrayValue
+        var relationResultArray: [String] = Array<String>()
+        
+        for res in relationResult {
+            relationResultArray.append(res.stringValue)
+        }
+       
+        return relationResultArray
+    }
+}
