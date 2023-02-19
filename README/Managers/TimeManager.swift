9edc049d7ce7ab3ab5c638a28a0dcbbfe2f0b249
myungsun <audtjs9987@naver.com>
2023-02-19 21:26:53 +0900

    feat: develop TimeManager class

--- README/Managers/TimeManager.swift
+++ README/Managers/TimeManager.swift
@@ -6,3 +6,22 @@
 //
 
 import Foundation
+
+class TimeManager {
+    static let shared = TimeManager()
+    
+    private init() {}
+    
+    func getRegDataToDateString(regData: Int) -> String{
+        var regDataString: String = String(regData)
+        
+        let monthRange = regDataString.index(regDataString.startIndex, offsetBy: 4) ... regDataString.index(regDataString.startIndex, offsetBy: 5)
+        let dayRange = regDataString.index(regDataString.startIndex, offsetBy: 6) ... regDataString.index(regDataString.startIndex, offsetBy: 7)
+        
+        let year = regDataString.prefix(4)
+        let month = regDataString[monthRange]
+        let day = regDataString[dayRange]
+        
+        return year + ". " + month + ". " + day
+    }
+}
