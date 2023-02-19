3e451ccd618ec361aa50d13e257efab4bcf1f9e5
myungsun <audtjs9987@naver.com>
2023-02-19 21:28:06 +0900

    feat: develop NetworkManager class

--- README/Managers/NetworkManager.swift
+++ README/Managers/NetworkManager.swift
@@ -20,12 +20,22 @@ class NetworkManager {
     private init() {}
     
     typealias RelationSearchCompletion = (Result<[String], NetworkError>) -> Void
+    typealias SearchResultCompletion = (Result<[NewsResult], NetworkError>) -> Void
     
     func fetchRelationSearch(searchTerm: String, completionHandler: @escaping RelationSearchCompletion) {
         let urlString = "\(SearchApi.requestUrl)\(SearchApi.relationSearchParam)\(searchTerm)"
         guard let encodedStr = urlString.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed) else { return }
         
         performRequest(with: encodedStr) { result in
+            completionHandler(result)
+        }
+    }
+    
+    func fetchSearchResults(searchTerm: String, completionHandler: @escaping SearchResultCompletion) {
+        let urlString = "\(SearchApi.requestUrl)\(SearchApi.searchParam)\(searchTerm)"
+        guard let encodedStr = urlString.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed) else { return }
+        
+        performSearchRequest(with: encodedStr) { result in
             completionHandler(result)
         }
     }
@@ -47,6 +57,31 @@ class NetworkManager {
             
             if let recentResultArray = ParseManager.shared.parseRelationResultJSON(safeData) {
                 completionHandler(.success(recentResultArray))
+            } else {
+                print("Parse 실패")
+                completionHandler(.failure(.parseError))
+            }
+        }
+        task.resume()
+    }
+    
+    private func performSearchRequest(with urlString: String, completionHandler: @escaping SearchResultCompletion) {
+        guard let url = URL(string: urlString) else { return }
+        let session = URLSession(configuration: .default)
+        let task = session.dataTask(with: url) { (data, response, error) in
+            if error != nil {
+                print(error!)
+                completionHandler(.failure(.networkingError))
+                return
+            }
+            
+            guard let safeData = data else {
+                completionHandler(.failure(.dataError))
+                return
+            }
+            
+            if let searchResultArray = ParseManager.shared.parseSearchResultJSON(safeData) {
+                completionHandler(.success(searchResultArray))
             } else {
                 print("Parse 실패")
                 completionHandler(.failure(.parseError))
