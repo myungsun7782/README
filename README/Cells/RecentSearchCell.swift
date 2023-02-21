c11571a0b970df5ab38b4bf4f1739ba061059b93
myungsun <audtjs9987@naver.com>
2023-02-21 20:18:43 +0900

    feat: compose RelationSearchCell view

--- README/Cells/RecentSearchCell.swift
+++ README/Cells/RecentSearchCell.swift
@@ -6,18 +6,36 @@
 //
 
 import UIKit
+import RxSwift
+import RxCocoa
 
 class RecentSearchCell: UITableViewCell {
-
+    // UILabel
+    @IBOutlet weak var recentSearchLabel: UILabel!
+    
+    // UIButton
+    @IBOutlet weak var deleteButton: UIButton!
+    
+    // RxSwift
+    // 외부에서 읽기만 가능하고 내부에서만 수정이 가능하도록 하는 코드
+    private(set) var disposeBag = DisposeBag()
+    
+    // Constants
+    let BUTTON_IMAGE: UIImage? = UIImage(systemName: "xmark", withConfiguration: UIImage.SymbolConfiguration(pointSize: 18))
+    
     override func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
-    }
-
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
+        deleteButton.setImage(BUTTON_IMAGE, for: .normal)
+        
     }
     
+    override func prepareForReuse() {
+        super.prepareForReuse()
+        disposeBag = DisposeBag()
+    }
+
+    
+    func setData(recentSearchTerm: String) {
+        recentSearchLabel.text = recentSearchTerm
+    }
 }
