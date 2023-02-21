bd993dbf45dd470f817ec8b6278fa978e94befe3
myungsun <audtjs9987@naver.com>
2023-02-21 20:25:53 +0900

    feat: develop detailNews view Model

--- README/ViewModels/DetailNewsVM.swift
+++ README/ViewModels/DetailNewsVM.swift
@@ -6,3 +6,36 @@
 //
 
 import Foundation
+import RxSwift
+
+class DetailNewsVM {
+    // Input
+    var input = Input()
+    
+    // Output
+    var output = Output()
+    
+    // Variables
+    var news: News!
+    var category: String!
+    var isClicked: Bool = false
+    var isClickedArray: [Bool] = [false, false, true, false, false]
+    
+    // Constants
+    
+    // RxSwift
+    let disposeBag = DisposeBag()
+
+    struct Input {
+        
+    }
+    
+    struct Output {
+        var isClicked = BehaviorSubject<Bool>(value: false)
+        var isClckedArray = BehaviorSubject<[Bool]>(value: [false, false, true, false, false])
+    }
+    
+    init() {
+
+    }
+}
