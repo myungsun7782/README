2b2784af564720539196938e70e873d51e580658
myungsun <audtjs9987@naver.com>
2023-02-21 20:25:32 +0900

    feat: develop SearchResult view model class

--- README/ViewModels/SearchResultVM.swift
+++ README/ViewModels/SearchResultVM.swift
@@ -6,3 +6,45 @@
 //
 
 import Foundation
+import RxSwift
+
+class SearchResultVM {
+    // Input
+    var input = Input()
+    
+    // Output
+    var output = Output()
+    
+    // Variables
+    var searchResult: String!
+    var searchResultArray: [NewsResult] = Array<NewsResult>()
+    
+    // Constants
+    
+    // RxSwift
+    let disposeBag = DisposeBag()
+
+    struct Input {
+
+    }
+    
+    struct Output {
+        var searchDone = PublishSubject<Void>()
+    }
+    
+    init() {
+
+    }
+    
+    func fetchData() {
+        NetworkManager.shared.fetchSearchResults(searchTerm: searchResult) { result in
+            switch result {
+            case .success(let newsResultArray):
+                self.searchResultArray = newsResultArray
+            case .failure(let error):
+                print(error.localizedDescription)
+            }
+            self.output.searchDone.onNext(())
+        }
+    }
+}
